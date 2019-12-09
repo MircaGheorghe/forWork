b2f1ba2b521aae677022a8e012efeb28e1a43aaf
Mirca Gheorghe <giony.mirca@mail.ru>
2019-12-09 23:36:18 +0200

    first commit

---
+++ include/test.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import urllib.request
+
+
+headers = {
+    'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+}
+
+base_url = 'https://makler.md/ro/real-estate/real-estate-for-sale/apartments-for-sale/an/680734'
+
+def parse_makler(base_url, headers):
+    session = requests.Session()
+    request = session.get(base_url, headers = headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        div_header = soup.find_all('div', attrs={'class': 'title clrfix'})
+        post_title = div_header[0].find('strong', attrs={'id': 'anNameData'}).text
+        post_price = div_header[0].find('div', attrs={'class': 'item_title_price'}).text
+        currency = post_price[-1:]
+        post_price = post_price[:-1].strip()
+        post_price = post_price.replace(' ', '')
+        print('Denumirea: ' + post_title + "\nPretul: " + post_price)
+
+
+        categories = soup.find_all('li', attrs={'class': 'pl'})
+        list_cat = []
+        for category in categories[1:]:
+            list_cat.append(category.find('a').text.strip())
+
+
+        print("\n\nImaginile: \n")
+        div_image = soup.find('div', id = "anItemData").find('div', attrs={'class': 'itmedia'})
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+
+        i = 0
+        for a in div_image.find_all("a"):
+            print(a['href'])
+            urllib.request.urlretrieve(a['href'], "img/{}.jpg".format(str(i)))
+            i += 1
+            img_count = i
+        content = soup.find('div', attrs={'class', 'ittext'}).text.strip()
+        print(content)
+
+        print("\n\nCaracteristica - Valoarea \n")
+        uls = soup.find_all('ul', attrs={'class': 'itemtable box-columns'})
+        specifications = {}
+        for ul in uls:
+            for li in ul.find_all('li'):
+                title = li.find("div", attrs={"class": "fields"}).text.strip()
+                value = li.find("div", attrs={"class": "values"}).text.strip()
+                specifications[title] = value
+                print(title + ' - ' + value)
+            print('------')
+
+        tower = soup.find("ul", attrs={'class', 'item-city'}).text.strip()
+        print(tower)
+        phone = soup.find("ul", attrs={'class', 'hlist clrfix'}).text.strip()
+        print(phone)
+        born = soup.find('div', attrs={'class', 'item_title_info'}).find_all('span')[0].text
+        print(born)
+        return post_title, post_price, list_cat, content, currency, born, img_count, specifications, phone, born
+    else:
+        print("Error")
+
+post_title, post_price, list_cat, content, currency, born, img_count, specifications, phone, born  = parse_makler(base_url, headers)
---
+++ selenium_test.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import include.test as test
+from time import sleep as sleep
+
+driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver")
+driver.get("https://makler.md/md/")
+driver.maximize_window()
+
+user_name = "37368035704"
+password = "gheorghe1703"
+
+#find login link
+login_block = driver.find_element_by_id('logInDiv')
+login_block.click()
+
+#wait 3 second and then find email input and typing user_name
+driver.implicitly_wait(3)
+element = driver.find_element_by_name('login')
+element.send_keys(user_name, Keys.ARROW_DOWN)
+
+#find password input and typing password
+element = driver.find_element_by_name('password')
+element.send_keys(password)
+
+#click to submit button and enter to site
+element = driver.find_element_by_class_name("popupRedButton")
+element.click()
+
+#wait two second and redirect to add
+sleep(2)
+driver.get("https://makler.md/md/an/web/add")
+
+#find select list and click to everyone
+print(test.list_cat[0])
+print(test.list_cat[1])
+print(test.born)
+
+#find and enter the name of post
+element = driver.find_element_by_id("editorName")
+element.send_keys(test.post_title)
+
+#find and enter the describtion of the current post
+element = driver.find_element_by_id("editorText")
+element.send_keys(test.content)
+
+# find and enter price
+element = driver.find_element_by_id("price")
+element.send_keys(test.post_price)
+
+#work with currency
+curency_tab = {
+    '$':'USD',
+    '₴':'UAH',
+    '€':'EUR',
+    'lei':'MDL'
+}
+sleep(1)
+if test.currency in curency_tab.keys():
+    get_currency_div = driver.find_element_by_class_name('newAdForm_radioBoxButtons')
+    elements = get_currency_div.find_elements_by_tag_name('label')
+    for element in elements:
+        if element.text == curency_tab[test.currency]:
+            element.click()
+
+#upload image
+sleep(1)
+element = driver.find_element_by_class_name("qq-upload-button")
+
+for i in range(test.img_count - 1):
+    elm = driver.find_element_by_xpath("//input[@type='file']")
+    elm.send_keys("C://Users/Gheorghe/OneDrive/Documents/Python/work/img/"+ str(i) +".jpg")
+
+
+element = driver.find_element_by_class_name('zend_form')
+#variabila ce contine elementele de pe pagina de incarcare a datelor
+labels = element.find_elements_by_class_name('newAdForm_boxFieldLabel')
+#variabila cu datele de pe pagina de luare a datelor
+keys = test.specifications.keys()
+
+#fell of the category inputs
+for label in labels:
+    for key in keys:
+        if key in label.text or key == label.text:
+            li = label.find_element_by_xpath('..')
+            select_ = li.find_elements_by_tag_name('select')
+            optgroup = li.find_elements_by_tag_name('optgroup')
+            if select_:
+                try:
+                    li.find_element_by_xpath("//select/optgroup/option[text()='" + test.specifications[key] +"']").click()
+                except:
+                    li.find_element_by_xpath("//select/option[text()='" + test.specifications[key] +"']").click()
+                break
+            input_ = li.find_element_by_tag_name('input')
+            if input_:
+                input_.send_keys(test.specifications[key].replace('m²', '').strip())
+                break
+
+#check the telephone checkbox
+element = driver.find_element_by_id('phone-37368035704').click()
+
+#plaseaza postarea
+element = driver.find_element_by_class_name('saveBtn').click()
